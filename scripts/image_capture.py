6937a7c09af5bce1a636db34c06613bdfdb42d0f
GitYangFan <103840315+GitYangFan@users.noreply.github.com>
2025-01-21 05:41:19 -0800

    Add files via upload

---
+++ scripts/image_capture.py
@@ -0,0 +1,35 @@
+import rospy
+from sensor_msgs.msg import Image
+import numpy as np
+#from cv_bridge import CvBridge
+import jetson_utils
+
+img = None
+#bridge = CvBridge()
+
+def image_callback(msg):
+    global img
+    # img_cv = bridge.imgmsg_to_cv2(msg, "bgr8")
+    img_cv = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
+    img = jetson_utils.cudaFromNumpy(img_cv)
+
+
+def get_image():
+    rospy.init_node('image_capture')
+    rospy.Subscriber('/camera/raw', Image, image_callback)
+    # Loop waiting to receive data
+    while not rospy.is_shutdown():
+        if img is not None:
+            print('Image captured successfully.')
+            return img
+        else:
+            print('failed to capture image.')
+        rospy.sleep(0.1)
+
+
+"""
+-------------------- test ----------------------------
+"""
+if __name__ == '__main__':
+    image = get_image()
+    print(image)
