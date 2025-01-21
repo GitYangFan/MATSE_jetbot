5d1cf94f12fe414de9a20bc380df80ec15690084
GitYangFan <103840315+GitYangFan@users.noreply.github.com>
2025-01-21 02:56:48 -0800

    Add files via upload

--- hardware/jetbot_camera.cpp
+++ hardware/jetbot_camera.cpp
@@ -1,138 +1,182 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 #include <ros/ros.h>
 
 #include <sensor_msgs/Image.h>
-#include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/CameraInfo.h>
 
 #include <jetson-utils/gstCamera.h>
 
 #include "image_converter.h"
 
-// globals
+
+
+// globals	
 gstCamera* camera = NULL;
 
 imageConverter* camera_cvt = NULL;
 ros::Publisher* camera_pub = NULL;
-ros::Publisher* camera_info_pub = NULL;
+
 
 // aquire and publish camera frame
-bool aquireFrame(sensor_msgs::CameraInfo& cam_info_msg)
+bool aquireFrame()
 {
-    float4* imgRGBA = NULL;
+	float4* imgRGBA = NULL;
 
-    // get the latest frame
-    if( !camera->CaptureRGBA((float**)&imgRGBA, 1000) )
-    {
-        ROS_ERROR("failed to capture camera frame");
-        return false;
-    }
+	// get the latest frame
+	if( !camera->CaptureRGBA((float**)&imgRGBA, 1000) )
+	{
+		ROS_ERROR("failed to capture camera frame");
+		return false;
+	}
 
-    // assure correct image size
-    if( !camera_cvt->Resize(camera->GetWidth(), camera->GetHeight(), IMAGE_RGBA32F) )
-    {
-        ROS_ERROR("failed to resize camera image converter");
-        return false;
-    }
+	// assure correct image size
+	if( !camera_cvt->Resize(camera->GetWidth(), camera->GetHeight(), IMAGE_RGBA32F) )
+	{
+		ROS_ERROR("failed to resize camera image converter");
+		return false;
+	}
 
-    // populate the Image message
-    sensor_msgs::Image img_msg;
-    if( !camera_cvt->Convert(img_msg, imageConverter::ROSOutputFormat, imgRGBA) )
-    {
-        ROS_ERROR("failed to convert camera frame to sensor_msgs::Image");
-        return false;
-    }
+//	// populate the message
+//	sensor_msgs::Image msg;
+//
+//	if( !camera_cvt->Convert(msg, imageConverter::ROSOutputFormat, imgRGBA) )
+//	{
+//		ROS_ERROR("failed to convert camera frame to sensor_msgs::Image");
+//		return false;
+//	}
+//
+//	// publish the message
+//	camera_pub->publish(msg);
+//	ROS_INFO("published camera frame");
+//	return true;
 
-    // populate the CameraInfo message
-    cam_info_msg.header = img_msg.header;
-    cam_info_msg.width = camera->GetWidth();
-    cam_info_msg.height = camera->GetHeight();
-    // Set camera intrinsic parameters here (example values)
-    cam_info_msg.K = {500, 0, cam_info_msg.width / 2.0,
-                      0, 500, cam_info_msg.height / 2.0,
-                      0, 0, 1};  // Example calibration matrix
+    // populate the message
+	sensor_msgs::Image msg;
+	sensor_msgs::CameraInfo cam_info_msg;
+	std::string camera_name;
+	std::string calib_path;
+	calib_path = "/home/jetbot/workspace/catkin_ws/src/jetbot_ros/ost.yaml";
+	camera_name = "narrow_stereo";
+    camera_calibration_parsers::readCalibrationYml(
+    calib_path, camera_name, cam_info_msg);
 
-    // publish the messages
-    camera_pub->publish(img_msg);
-    camera_info_pub->publish(cam_info_msg);
+	//ROS_INFO(cam_info_msg);
 
-    ROS_INFO("published camera frame and camera info");
-    return true;
+	if( !camera_cvt->Convert(msg, sensor_msgs::image_encodings::BGR8, imgRGBA) )
+	{
+		ROS_ERROR("failed to convert camera frame to sensor_msgs::Image");
+		return false;
+	}
+
+	// header
+	static uint32_t seq = 0;
+	static const char* frame_id = camera->GetResource().c_str();
+	msg.header.seq = seq;
+	msg.header.stamp = ros::Time::now();
+	msg.header.frame_id = frame_id;
+	seq++;
+
+	cam_info_msg.header.frame_id = msg.header.frame_id;
+    cam_info_msg.header.stamp = msg.header.stamp;
+	// publish the message
+	cam_pub->publish(msg, cam_info_msg);
+	ROS_DEBUG("published camera frame");
+	return true;
 }
+
 
 // node main loop
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "jetbot_camera");
+	ros::init(argc, argv, "jetbot_camera");
+ 
+	ros::NodeHandle nh;
+	ros::NodeHandle private_nh("~");
 
-    ros::NodeHandle nh;
-    ros::NodeHandle private_nh("~");
+	/*
+	 * retrieve parameters
+	 */
+	std::string camera_device = "0";	// MIPI CSI camera by default
 
-    /*
-     * retrieve parameters
-     */
-    std::string camera_device = "0";	// MIPI CSI camera by default
+	private_nh.param<std::string>("device", camera_device, camera_device);
+	
+	ROS_INFO("opening camera device %s", camera_device.c_str());
 
-    private_nh.param<std::string>("device", camera_device, camera_device);
+	
+	/*
+	 * open camera device
+	 */
+	camera = gstCamera::Create(camera_device.c_str());
 
-    ROS_INFO("opening camera device %s", camera_device.c_str());
+	if( !camera )
+	{
+		ROS_ERROR("failed to open camera device %s", camera_device.c_str());
+		return 0;
+	}
 
 
-    /*
-     * open camera device
-     */
-    camera = gstCamera::Create(camera_device.c_str());
+	/*
+	 * create image converter
+	 */
+	camera_cvt = new imageConverter();
 
-    if( !camera )
-    {
-        ROS_ERROR("failed to open camera device %s", camera_device.c_str());
-        return 0;
-    }
+	if( !camera_cvt )
+	{
+		ROS_ERROR("failed to create imageConverter");
+		return 0;
+	}
 
-    /*
-     * create image converter
-     */
-    camera_cvt = new imageConverter();
 
-    if( !camera_cvt )
-    {
-        ROS_ERROR("failed to create imageConverter");
-        return 0;
-    }
+	/*
+	 * advertise publisher topics
+	 */
+	ros::Publisher camera_publisher = private_nh.advertise<sensor_msgs::Image>("raw", 2);
+	camera_pub = &camera_publisher;
 
-    /*
-     * advertise publisher topics
-     */
-    ros::Publisher camera_publisher = private_nh.advertise<sensor_msgs::Image>("raw", 2);
-    ros::Publisher camera_info_publisher = private_nh.advertise<sensor_msgs::CameraInfo>("camera_info", 2);
-    camera_pub = &camera_publisher;
-    camera_info_pub = &camera_info_publisher;
 
-    /*
-     * start the camera streaming
-     */
-    if( !camera->Open() )
-    {
-        ROS_ERROR("failed to start camera streaming");
-        return 0;
-    }
+	/*
+	 * start the camera streaming
+	 */
+	if( !camera->Open() )
+	{
+		ROS_ERROR("failed to start camera streaming");
+		return 0;
+	}
 
-    /*
-     * initialize CameraInfo message
-     */
-    sensor_msgs::CameraInfo camera_info_msg;
-    camera_info_msg.header.frame_id = "camera_frame";  // Frame ID for tf
-    camera_info_msg.distortion_model = "plumb_bob";
-    camera_info_msg.D = {0, 0, 0, 0, 0};  // Example distortion coefficients
 
-    /*
-     * start publishing video frames
-     */
-    while( ros::ok() )
-    {
-        aquireFrame(camera_info_msg);
-        ros::spinOnce();
-    }
+	/*
+	 * start publishing video frames
+	 */
+	while( ros::ok() )
+	{
+		//if( raw_pub->getNumSubscribers() > 0 )
+			aquireFrame();
 
-    delete camera;
-    return 0;
+		ros::spinOnce();
+	}
+
+	delete camera;
+	return 0;
 }
